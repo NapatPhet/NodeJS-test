e3c1b2f5d92a11cd4c004ddb8f8823ad051ee008
Natty_UniWorky <nnapatsiri@gmail.com>
2023-05-27 15:04:15 +0700

    try to use rounter

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const chalk = require('chalk')
 const debug = require('debug')
 const morgan = require('morgan')
+const path = require('path')
+const productRounter = express.Router();
 const app = express();
 
-
 // Middleware
-app.use(express.json());
-
+app.use(express.static(path.join(__dirname,"/public/")));
 app.use(morgan('combined'))
 
+app.set("views","./src/views");
+app.set("view engine", "ejs")
+productRounter.route("/").get((res,req) =>  {
+  res.setEncoding("Hello World this is product")
+})
+app.use("/product", productRounter)
 // Route Handler
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render('index',{username: 'Natty'});
+});
 
 // Start the server
 app.listen(4000, () => {
